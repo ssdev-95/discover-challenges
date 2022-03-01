0c9476de2006f2876ab998ec6a27f42f988792a1
xSallus <souza95salomao@gmail.com>
2022-03-01 21:37:55 +0000

    [ADD] New challenge: social-tree

---
+++ social-tree/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tree/>
+</template>
+
+<script setup>
+import Tree from './components/tree.vue'
+</script>
+
+<style lang="scss">
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+#app {
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
---
+++ social-tree/src/components/tree.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="link-tree">
+  <div class="user-pic">
+		<img src="" alt="xSallus" />
+	</div>
+	<div class="user-links"></div>
+	<div class="user-social">
+		<img src="../assets/twitter.svg" alt="twitter" />
+		<img src="../assets/twitch.svg" alt="twitch" />
+		<img src="../assets/instagram.svg" alt="instagram" />
+		<img src="../assets/linkedin.svg" alt="linkedin" />
+		<img src="../assets/github.svg" alt="github" />
+	</div>
+</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+@import "@/styles/tree.scss";
+
+.link-tree {
+	@include link-tree;
+}
+
+.user-pic {
+	@include user-pic;
+}
+
+.user-links {
+	@include user-links;
+}
+
+.user-social {
+	@include user-social;
+}
+</style>
