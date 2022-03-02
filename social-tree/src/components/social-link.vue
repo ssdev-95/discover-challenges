e79dd8d8645a363e5487e06b90a8868a75078790
xSallus <souza95salomao@gmail.com>
2022-03-02 04:06:11 +0000

    [ADD] Social link component

---
+++ social-tree/src/components/social-link.vue
@@ -0,0 +1,16 @@
+<template>
+<a
+	v-if="url"
+	:href="url"
+	target="__blank"
+>
+  <img :src="icon" :alt="alt" />
+</a>
+<button v-else disabled >
+	<img :src="icon" :alt="alt" />
+</button>
+</template>
+
+<script setup >
+const { alt, url, icon} = defineProps(['alt', 'url', 'icon'])
+</script>
--- social-tree/src/components/tree.vue
+++ social-tree/src/components/tree.vue
@@ -11,43 +11,51 @@
 	</div>
 	<p>&lt;{{username}}&nbsp;&bsol;&gt;</p>
 	<div class="user-links">
-		<a class="link-card" href="#" >
-			<span>LINK</span>
+		<a
+			class="link-card" 
+			href="https://rocketseat.com.br/ignite"
+			target="__blank"
+		>
+			<span>Dê um boost na sua carreira | Ignite</span>
+		</a>
+		<a
+			class="link-card"
+			href="https://lp.rocketseat.com.br/inscricao/showmethecode?utm_source=google_ads&utm_medium=cpc&utm_term=srch&gclid=CjwKCAiApfeQBhAUEiwA7K_UHxJFwm_EODjdJ4vPOUC99LI5Ly3qEqJC3AjV4U_KOIa5NGrC7M5OJRoCVf4QAvD_BwE"
+			target="__blank"
+		>
+			<span>Imscreva-se | Show Me The Code</span>
 		</a>
 		<a class="link-card" href="#" >
-			<span>LINK</span>
+			<span>LINK 3</span>
 		</a>
 		<a class="link-card" href="#" >
-			<span>LINK</span>
-		</a>
-		<a class="link-card" href="#" >
-			<span>LINK</span>
+			<span>LINK 4</span>
 		</a>
 	</div>
 	<div class="user-social">
 		<SocialLink
 			:url="social.twitter"
-			icon="@/assets/twitter.svg"
+			icon="https://raw.githubusercontent.com/xSallus/discover-challenges/main/social-tree/src/assets/twitter.svg"
 			alt="twitter"
 		/>
 		<SocialLink
 			:url="social.twitch"
-			icon="@/assets/twitch.svg"
+			icon="https://raw.githubusercontent.com/xSallus/discover-challenges/main/social-tree/src/assets/twitch.svg"
 			alt="twitch" 
 		/>
 		<SocialLink
 			:url="social.instagram"
-			icon="@/assets/instagram.svg"
+			icon="https://raw.githubusercontent.com/xSallus/discover-challenges/main/social-tree/src/assets/instagram.svg"
 			alt="instagram"
 		/>
 		<SocialLink
 			:url="social.linkedin"
-			icon="@/assets/linkedin.svg"
+			icon="https://raw.githubusercontent.com/xSallus/discover-challenges/main/social-tree/src/assets/linkedin.svg"
 			alt="linkedin"
 		/>
 		<SocialLink
 			:url="social.github"
-			icon="@/assets/github.svg"
+			icon="https://raw.githubusercontent.com/xSallus/discover-challenges/main/social-tree/src/assets/github.svg"
 			alt="github"
 		/>
 	</div>
