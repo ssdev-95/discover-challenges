563ef57b52bf532725fb0da13e567cd895f27ea8
xSallus <souza95salomao@gmail.com>
2022-03-02 16:01:29 +0000

    [UPDATE] Global styles

--- countdown/src/App.vue
+++ countdown/src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<main>
+		<h1>READY TO LAUNCH IN...</h1>
+		<Countdown />
+		<p>Subscribe to know more about what's coming on :D</p>
+		<button class="subscribe">Subscribe</button>
+	</main>
+	<img
+		class="rocket"
+		src="https://raw.githubusercontent.com/xSallus/discover-challenges/main/countdown/src/assets/rocket.svg"
+		alt="Rocket"
+	/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Countdown from './components/countdown.vue'
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  $Black: #4D4C59;
+  $Purple: #6C63FF;
+	$LightGrey: #C8C8C8;
+  $TextColor: #9C9AB6;
+	$Background: $Black;
 }
+
+#app {
+  font-family: 'Poppins', sans-serif;
+	width: 100vw;
+	height: 100vh;
+	padding: 1rem;
+
+	background-color: $Background;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	& > main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 2rem;
+	}
+
+	@media(min-width:1024px) {
+		padding: 3rem;
+	}
+}
+
+.rocket {}
+
+.subscribe {}
 </style>
