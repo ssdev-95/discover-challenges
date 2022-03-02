5ab5c669e5369447be4b1f6a7d12c9bc748c9d77
xSallus <souza95salomao@gmail.com>
2022-03-02 03:21:21 +0000

    [UPDATE] Assets

--- social-tree/src/App.vue
+++ social-tree/src/App.vue
@@ -1,23 +1,89 @@
 <template>
-  <Tree/>
+<div
+	class="wrapper"
+	:class="{'dark-mode': isDarkMode}"
+>
+  <button @click="toggleTheme">
+		<span v-if="isDarkMode.value">Dark</span>
+		<span v-else>Light</span>
+	</button>
+  <Tree />
+</div>
 </template>
 
 <script setup>
-import Tree from './components/tree.vue'
+import { ref, provide, onBeforeMount } from 'vue'
+import Tree from '@/components/tree.vue'
+
+let isDarkMode = ref(true)
+function toggleTheme() {
+	const state = !isDarkMode.value
+
+	isDarkMode.value = state
+	localStorage.setItem('@Tree:theme', state?1:0)
+}
+
+onBeforeMount(() => {
+	isDarkMode.value = !!localStorage.getItem('@Tree:theme')
+})
+
+provide('IS-DARK-MODE', isDarkMode)
 </script>
 
 <style lang="scss">
+$BG-LIGHT: #f0f2f5;
+$BG-DARK: #000000;
+$BT-PRIMARY: #41b883;
+
 * {
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
 }
 
-#app {
+.wrapper {
 	height: 100vh;
 	width: 100vw;
 	display: flex;
 	align-items: center;
 	justify-content: center;
+
+	background: $BG-LIGHT;
+
+	& > button {
+		padding: 0.25rem 0.5rem;
+		width: 3rem;
+		border-radius: 50px;
+		background: transparent;
+
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+
+		border: 2px solid $BT-PRIMARY;
+
+		transition: all 0.5s ease;
+
+		&:active {
+			filter: invert(10%);
+		}
+
+		& span {
+			color: $BT-PRIMARY;
+		}
+	}
+
+	&.dark-mode {
+		background: $BG-DARK;
+
+		& > button {
+			background: $BT-PRIMARY;
+			border: none;
+
+			& span {
+				color: $BG-LIGHT;
+			}
+		}
+	}
 }
 </style>
--- social-tree/src/components/tree.vue
+++ social-tree/src/components/tree.vue
@@ -1,20 +1,76 @@
 <template>
-<div class="link-tree">
+<div
+	class="link-tree"
+	:class="{'dark-mode': isDarkMode}"
+>
   <div class="user-pic">
-		<img src="" alt="xSallus" />
+		<img
+			:src="avatar_url"
+			alt="xSallus"
+		/>
 	</div>
-	<div class="user-links"></div>
+	<p>&lt;{{username}}&nbsp;&bsol;&gt;</p>
+	<div class="user-links">
+		<a class="link-card" href="#" >
+			<span>LINK</span>
+		</a>
+		<a class="link-card" href="#" >
+			<span>LINK</span>
+		</a>
+		<a class="link-card" href="#" >
+			<span>LINK</span>
+		</a>
+		<a class="link-card" href="#" >
+			<span>LINK</span>
+		</a>
+	</div>
 	<div class="user-social">
-		<img src="../assets/twitter.svg" alt="twitter" />
-		<img src="../assets/twitch.svg" alt="twitch" />
-		<img src="../assets/instagram.svg" alt="instagram" />
-		<img src="../assets/linkedin.svg" alt="linkedin" />
-		<img src="../assets/github.svg" alt="github" />
+		<SocialLink
+			:url="social.twitter"
+			icon="@/assets/twitter.svg"
+			alt="twitter"
+		/>
+		<SocialLink
+			:url="social.twitch"
+			icon="@/assets/twitch.svg"
+			alt="twitch" 
+		/>
+		<SocialLink
+			:url="social.instagram"
+			icon="@/assets/instagram.svg"
+			alt="instagram"
+		/>
+		<SocialLink
+			:url="social.linkedin"
+			icon="@/assets/linkedin.svg"
+			alt="linkedin"
+		/>
+		<SocialLink
+			:url="social.github"
+			icon="@/assets/github.svg"
+			alt="github"
+		/>
 	</div>
 </div>
 </template>
 
-<script setup></script>
+<script setup >
+import { inject } from 'vue'
+import SocialLink from '@/components/social-link.vue'
+
+const nickname = 'xsallus'
+const username = 'Salomao S.'
+const avatar_url = `https://github.com/${nickname}.png`
+const social = {
+	twitch: null,
+	twitter: `https://mobile.twitter.com/${nickname}_dev`,
+	instagram: `https://www.instagram.com/${nickname}.dev/`,
+	linkedin: 'https://linkedin.com/in-salomao-vasconcelos',
+	github: `https://github.com/${nickname}`
+}
+
+const isDarkMode = inject('IS-DARK-MODE')
+</script>
 
 <style lang="scss">
 @import "@/styles/tree.scss";
