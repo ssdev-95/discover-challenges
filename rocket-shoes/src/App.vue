e8205c2efeab6301c07e5bf3e39b2a9c9af4785f
Salomao S <souza95salomao@gmail.com>
2022-05-16 21:47:17 +0000

    [UPDATE] Add most of content

---
+++ rocket-shoes/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+import Main from './components/main.vue'
+</script>
+
+<template>
+  <Header />
+  <Main />
+	<Footer />
+</template>
+
+<style lang="scss">
+@import "./colors.scss";
+
+* {
+  font-family:Roboto, sans-serif;
+  -webkit-font-smoothing:antialiased;
+  -moz-osx-font-smoothing:grayscale;
+  
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+}
+
+#app {
+	width:100vw;
+	min-height:100vh;
+	background:$background;
+}
+</style>
---
+++ rocket-shoes/src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+<header>
+	<img
+		src="/assets/logo.svg"
+		alt="A red shoe icon"
+	/>
+	<p>MAN</p>
+	<p>WOMAN</p>
+	<p>KIDS</p>
+	<p>CUSTOMIZE</p>
+	<label>
+		<img
+			src="/assets/icon-search.svg"
+			alt="A search icon"
+		/>
+		<input />
+	</label>
+	<img
+		src="/assets/icon-bag.svg"
+		alt="A bag icon"
+	/>
+</header>
+</template>
+
+<style scoped lang="scss">
+header {
+	width:1480px;
+	max-width: 98vw;
+	height:4rem;
+
+	display:flex;
+	justify-content:space-between;
+	align-items:center;
+}
+</style>
