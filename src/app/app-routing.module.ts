f2c435d2d60cca981a1f8c2da8e45712e711bdff
Salomao S <souza95salomao@gmail.com>
2022-11-30 17:49:02 +0000

    update: setup initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { CountdownComponent } from './countdown/countdown.component'
-import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [{
 	path: 'countdown',
-	component: CountdownComponent
+	component: CountdownComponent,
+	title: 'Countdown | DCChallenges'
 }, {
 	path: '',
-	component: HomeComponent
+	component: HomeComponent,
+	title: 'Home | DCChallenges'
 }];
 
 @NgModule({
