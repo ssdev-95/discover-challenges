1da5d13d25bd342770d561e5b008e10b9dd38663
Salomao S <souza95salomao@gmail.com>
2022-11-30 17:49:48 +0000

    update: countdown challenge finshed

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./countdown.component.css']
 })
 export class CountdownComponent {
+	timeout:NodeJS.Timer = {} as NodeJS.Timer
+	initTime = 40 * 60
 
+	timer = {
+		days: 0,
+		hours: 0,
+		minutes: 0,
+		seconds: 0
+	}
+
+	ngOnInit() {
+		this.timeout = setInterval(() => {
+			if(this.initTime > 0) {
+
+  			this.timer = {
+					...this.timer,
+					minutes: Math.floor(this.initTime / 60),
+					seconds: Math.round(this.initTime % 60)
+				}
+
+				this.initTime -= 1
+			} else {
+				clearTimeout(this.timeout)
+				this.initTime = 40 * 60
+			}
+		}, 1000)
+	}
+
+	ngOnDestroy() {
+		clearTimeout(this.timeout)
+	}
 }
